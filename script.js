065e1d8cba6e10c766b6356e7bc3b0c1e004e04b
Sharafath_V <142240872+sharafath07@users.noreply.github.com>
2024-10-06 08:09:40 +0000

    made protfolio

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+let portfolio = document.querySelector('#portfolio')
+
+next.addEventListener('click', function(){
+  let items = document.querySelectorAll('.item')
+  document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+  let items = document.querySelectorAll('.item')
+  document.querySelector('.slide').prepend(items[items.length - 1])
+})
+
+document.addEventListener('keydown', function(event){
+  if(event.key === "ArrowRight"){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+  } else if(event.key === "ArrowLeft"){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+  }
+})
+
 window.addEventListener("load", function () {
     const loadingScreen = document.querySelector(".loader");
     const mainContent = document.querySelector(".main-content");
